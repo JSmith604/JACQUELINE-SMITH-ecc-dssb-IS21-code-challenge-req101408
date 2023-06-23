04b8f2b503ca1ccdad11ea4f36e38835a807febe
JACQUELINE <jacqueline.smith@thinkific.com>
2023-06-23 15:23:16 -0700

    Add Axios to project

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
+const port = 3001; // Choose any available port number
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const port = 3001; // Choose any available port number
+app.get('/api', (req, res) => {
+  const data = 'This is the data from the backend!'
+  res.json(data)
+})
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
