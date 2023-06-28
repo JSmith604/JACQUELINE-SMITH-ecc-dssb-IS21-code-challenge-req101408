42db75ded72b8598df6f5d3e53f2361b5abf8fc1
JACQUELINE <jacqueline.smith@thinkific.com>
2023-06-28 14:35:44 -0700

    Add 200 error code when the get request succeeds

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,31 @@ const port = 3001; // Choose any available port number
 const app = express();
 const mockData = require('./data/mockData');
 const bodyParser = require('body-parser');
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json(mockData.products);
+  try {
+    res.status(200).json(mockData.products);
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.get('/api', (req, res) => {
-  res.json(mockData.products);
-})
+  try {
+    res.status(200).json(mockData.products);
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.post('/api/add-entry', (req, res) => {
   const newEntry = req.body;
   mockData.products.push(newEntry);
-  res.status(201).json({ message: 'Entry added successfully' });
+  res.status(201).json({ error: 'Entry added successfully' });
 });
 
 app.listen(port, () => {
