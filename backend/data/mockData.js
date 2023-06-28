77ab81b4f67e22716056e857644f83f2aa53f1fa
JACQUELINE <jacqueline.smith@thinkific.com>
2023-06-27 21:57:18 -0700

    Add more products to mockData

--- backend/data/mockData.js
+++ backend/data/mockData.js
@@ -22,6 +22,95 @@ const mockData = {
       lifecycleState: "In development",
       location: "https://github.com/bcgov/CIOT"
     },
+    {
+      productId: 3,
+      productName: "Product 3",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/07/05",
+      methodology: "Waterfall",
+      lifecycleState: "In development",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    {
+      productId: 4,
+      productName: "Product 4",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/07/15",
+      methodology: "Agile",
+      lifecycleState: "In maintenance",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    
+    {
+      productId: 5,
+      productName: "Product 5",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/07/20",
+      methodology: "Waterfall",
+      lifecycleState: "Marked for modernization",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    {
+      productId: 6,
+      productName: "Product 6",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/07/25",
+      methodology: "Agile",
+      lifecycleState: "Marked for modernization",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    {
+      productId: 7,
+      productName: "Product 7",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/08/01",
+      methodology: "Waterfall",
+      lifecycleState: "Marked for modernization",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    {
+      productId: 8,
+      productName: "Product 8",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/08/05",
+      methodology: "Agile",
+      lifecycleState: "Marked for modernization",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    {
+      productId: 9,
+      productName: "Product 9",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/08/10",
+      methodology: "Agile",
+      lifecycleState: "Marked for modernization",
+      location: "https://github.com/bcgov/CIOT"
+    },
+    {
+      productId: 10,
+      productName: "Product 10",
+      productOwnerName: "Jane Smith",
+      developers: ["Mia Cooper", "Alexandra Wright", "Olivia Mitchell", "Benjamin Price", "Charlotte Morgan"],
+      scrumMasterName: "Lucas Hughes",
+      startDate: "2023/08/15",
+      methodology: "Waterfall",
+      lifecycleState: "Marked for modernization",
+      location: "https://github.com/bcgov/CIOT"
+    },
   ]
 };
 
